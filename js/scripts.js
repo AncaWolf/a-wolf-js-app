ebec98e81062e94af130c817f60024b3fa58f62d
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-07-17 20:19:12 +0200

    added variable pokemonList
    created 3 objects to array and gave each 3 values: name, height and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World!');
-var favouriteFood = 'Aubergine Salad';
-document.write(favouriteFood);
+let pokemonList = [
+    {name: "Bulbasaur", height: 70, type: ["grass", "poison"]},
+    {name: "Ivysaur", height: 100, type: ["grass", "poison"]},
+    {name: "Venusaur", height: 200, type: ["grass", "poison"]}
+];
