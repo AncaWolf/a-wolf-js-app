7eeaab716db750271f9163a5e0baa9d6e5056938
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-07-19 19:15:29 +0200

    added "for" loop and conditional for height

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ let pokemonList = [
     {name: "Ivysaur", height: 100, types: ["grass", "poison"]},
     {name: "Venusaur", height: 200, types: ["grass", "poison"]}
 ];
+for (let i = 0; i < pokemonList.length; i++)
+     if (pokemonList[i].height > 150) {
+    document.write(pokemonList[i].name + " " + "(Height: " + pokemonList[i].height + ") - " + "Wow, that is big!" + "\n"); 
+     }
+    else { document.write(pokemonList[i].name + "(Height: " + pokemonList[i].height + ") " + "\n")
+ }
+
