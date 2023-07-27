6639c03a4bbb09fbf4554764625c47b8b2863d0b
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-07-27 19:24:46 +0200

    Update script
    Task 1.5
    created IIFE pokemonRepository and added more pokemons to the list
    added functions: add and getAll to access objects inside IIFE
    changed for loop into forEach loop
    deleted old code

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,55 @@
-let pokemonList = [
-    {name: "Bulbasaur", height: 70, types: ["grass", "poison"]},
-    {name: "Ivysaur", height: 100, types: ["grass", "poison"]},
-    {name: "Venusaur", height: 200, types: ["grass", "poison"]}
-];
-for (let i = 0; i < pokemonList.length; i++)
-     if (pokemonList[i].height > 150) {
-    document.write(pokemonList[i].name + " " + "(Height: " + pokemonList[i].height + ") - " + "Wow, that is big!" + "\n"); 
-     }
-    else { document.write(pokemonList[i].name + "(Height: " + pokemonList[i].height + ") " + "\n")
- }
+// creating IIFE
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ["grass", "poison"]
+        },
+        {
+            name: 'Ivysaur',
+            height: 1,
+            types: ["grass", "poison"]
+        },
+        {
+            name: 'Venusaur',
+            height: 2,
+            types: ["grass", "poison"]
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: ["fire"]
+        },
+        {
+            name: 'Charmeleon',
+            height: 1.1,
+            types: ["fire"]
+        }
+    ]
+
+    function getAll () {
+        return pokemonList;
+    }
+// add function that allows only object data types to be added
+    function add (pokemon) {
+        if (typeof pokemon === "object" && "name" in pokemon) {
+        pokemonList.push(pokemon);
+        }else{
+            console.log("wrong input for pokemon list")
+        }
+    }
+    return {
+        getAll: getAll,
+        add: add
+    }
+
+})()
+
+// accesing IIFE objects
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon.name + ' has a height of: ' + pokemon.height + "<br>");
+});
+
 
